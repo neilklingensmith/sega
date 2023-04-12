a5b0864645a4b9088236bfbf38e2f036a7defb8f
Neil Klingensmith <neil@cs.luc.edu>
2023-04-12 12:04:01 -0500

    Adding MIT license to files

--- src/controller.h
+++ src/controller.h
@@ -1,3 +1,31 @@
+/*
+ * Sega Genesis Base Project
+ *
+ * Copyright (c) 2023 Neil Klingensmith
+ * neil@cs.luc.edu
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+
 #ifndef __CONTROLLER_H
 #define __CONTROLLER_H
 
@@ -6,13 +34,13 @@
 
 
 
-#define CONTROLLER_SELECT_PRESSED(x)  ((x>>7) & 1)
-#define CONTROLLER_A_PRESSED(x)       ((x>>6) & 1)
-#define CONTROLLER_C_PRESSED(x)       ((x>>5) & 1)
-#define CONTROLLER_B_PRESSED(x)       ((x>>4) & 1)
-#define CONTROLLER_RIGHT_PRESSED(x)   ((x>>3) & 1)
-#define CONTROLLER_LEFT_PRESSED(x)    ((x>>2) & 1)
-#define CONTROLLER_DOWN_PRESSED(x)    ((x>>1) & 1)
-#define CONTROLLER_UP_PRESSED(x)      ((x>>0) & 1)
+#define CONTROLLER_SELECT_PRESSED(x)  ((~x>>7) & 1)
+#define CONTROLLER_A_PRESSED(x)       ((~x>>6) & 1)
+#define CONTROLLER_C_PRESSED(x)       ((~x>>5) & 1)
+#define CONTROLLER_B_PRESSED(x)       ((~x>>4) & 1)
+#define CONTROLLER_RIGHT_PRESSED(x)   ((~x>>3) & 1)
+#define CONTROLLER_LEFT_PRESSED(x)    ((~x>>2) & 1)
+#define CONTROLLER_DOWN_PRESSED(x)    ((~x>>1) & 1)
+#define CONTROLLER_UP_PRESSED(x)      ((~x>>0) & 1)
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,34 @@
+/*
+ * Sega Genesis Base Project
+ *
+ * Copyright (c) 2023 Neil Klingensmith
+ * neil@cs.luc.edu
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 #include <stdint.h>
 #include "printfuncs.h"
 #include "gfx.h"
-
+#include "controller.h"
 
 void LoadPalettes();
 void LoadPatterns();
@@ -13,7 +40,7 @@ extern const uint32_t fontPallete[50][8];
 
 void wait() {
     unsigned int k;
-    for(k = 0; k < 50000; k++) {
+    for(k = 0; k < 10000; k++) {
         asm("nop");
         asm("nop");
         asm("nop");
@@ -21,7 +48,7 @@ void wait() {
 }
 
 void main() {
-    uint16_t k, x = 0, y = 0;
+    uint16_t k, x = 5, y = 5;
     uint8_t SACBRLDU; // Controller state
 
     LoadPalettes();
@@ -37,22 +64,23 @@ void main() {
     while(1){
 //        printString(x,y, "Hello Dave");
         SACBRLDU = readController();
-        rprintf("SACBRLDU = 0x%x\n", (int)(SACBRLDU&0xff));
+//        rprintf("SACBRLDU = 0x%x\n", (int)(SACBRLDU&0xff));
 //        rprintf("Tile @ (5,5) is %d\n", (int)readTile(3,5));
 
-        if(SACBRLDU & 1<<0) {
+        if(CONTROLLER_UP_PRESSED(SACBRLDU)) {
             y--;
         }
-        if(SACBRLDU & 1<<1) {
+        if(CONTROLLER_DOWN_PRESSED(SACBRLDU)) {
             y++;
         }
-        if(SACBRLDU & 1<<2) {
+        if(CONTROLLER_LEFT_PRESSED(SACBRLDU)) {
             x--;
         }
-        if(SACBRLDU & 1<<3) {
+        if(CONTROLLER_RIGHT_PRESSED(SACBRLDU)) {
             x++;
         }
 
+        printString(x, y, "NEIL");
         wait();
     }
 }
